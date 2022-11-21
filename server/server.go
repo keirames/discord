05733aae335ebbbae67a04c2daf18e36a56cec6b
khang nguyen <keirames.l3@gmail.com>
2022-11-21 20:52:12 +0700

    I'am not listening to ws anymore

--- server/server.go
+++ server/server.go
@@ -14,7 +14,6 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
-	"github.com/gorilla/websocket"
 	_ "github.com/lib/pq"
 )
 
@@ -49,29 +48,29 @@ import (
 // 	fmt.Println(sql)
 // 	fmt.Printf("%+v\n", newMessages)
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// 	CheckOrigin:     func(r *http.Request) bool { return true },
+// }
 
-func reader(conn *websocket.Conn) {
-	for {
-		// read in a message
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		// print out that message for clarity
-		log.Println(string(p))
+// func reader(conn *websocket.Conn) {
+// 	for {
+// 		// read in a message
+// 		messageType, p, err := conn.ReadMessage()
+// 		if err != nil {
+// 			log.Println(err)
+// 			return
+// 		}
+// 		// print out that message for clarity
+// 		log.Println(string(p))
 
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
+// 		if err := conn.WriteMessage(messageType, p); err != nil {
+// 			log.Println(err)
+// 			return
+// 		}
+// 	}
+// }
 
 func main() {
 	config.LoadEnv()
@@ -93,21 +92,21 @@ func main() {
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
-	router.Handle("/ws", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ws, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-		}
+	// router.Handle("/ws", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	ws, err := upgrader.Upgrade(w, r, nil)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
 
-		log.Println("Client Connected")
-		err = ws.WriteMessage(1, []byte("Hi Client!"))
-		if err != nil {
-			log.Println(err)
-		}
-		// listen indefinitely for new messages coming
-		// through on our WebSocket connection
-		reader(ws)
-	}))
+	// 	log.Println("Client Connected")
+	// 	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	// listen indefinitely for new messages coming
+	// 	// through on our WebSocket connection
+	// 	reader(ws)
+	// }))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
