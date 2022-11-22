88573f09c31de89af4a5a6425c58758ee69eba19
khang nguyen <keirames.l3@gmail.com>
2022-11-22 23:24:33 +0700

    play with keyboard

--- rn-mobile/src/modules/chat/chat-box.tsx
+++ rn-mobile/src/modules/chat/chat-box.tsx
@@ -1,5 +1,12 @@
 import { Provider, useAtom } from 'jotai';
 import React, { useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 import { messagesAtom, roomIdAtom, userIdAtom } from './atoms';
 import { InputToolbar } from './input-toolbar';
@@ -12,17 +19,46 @@ interface Props {
   messages: Message[];
 }
 
-export const ChatBox: React.FC<Props> = (props) => {
-  const { userId, roomId } = props;
-
+export const ChatBox = () => {
   return (
-    <Provider
-      initialValues={[
-        [userIdAtom, userId],
-        [roomIdAtom, roomId],
-      ]}>
-      <MessageContainer />
-      <InputToolbar />
-    </Provider>
+    <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.chatContainer} behavior="padding">
+        <Text>chat container</Text>
+      </KeyboardAvoidingView>
+      <View style={styles.inputContainer}>
+        <Text>tool box</Text>
+        <TextInput />
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { height: '100%', borderWidth: 1, borderColor: 'pink' },
+  chatContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  inputContainer: {
+    position: 'relative',
+    bottom: 0,
+  },
+});
+
+// export const ChatBox: React.FC<Props> = (props) => {
+//   const { userId, roomId } = props;
+
+//   return (
+//     <Provider
+//       initialValues={[
+//         [userIdAtom, userId],
+//         [roomIdAtom, roomId],
+//       ]}>
+//       <MessageContainer />
+//       <InputToolbar />
+//     </Provider>
+//   );
+// };
