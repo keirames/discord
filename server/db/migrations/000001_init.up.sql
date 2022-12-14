4702da2441b79d33429b2aa3aba42f8e743096ea
khang nguyen <keirames.l3@gmail.com>
2022-12-14 22:38:41 +0700

    new schema voice server

--- server/db/migrations/000001_init.up.sql
+++ server/db/migrations/000001_init.up.sql
@@ -1,31 +1,28 @@
 create table users (
 	id uuid primary key default uuid_generate_v4(),
-	name varchar(50) not null
+	name varchar(50) not null,
+	created_at timestamp with time zone default now(),
 );
 
-create table rooms (
+create table servers (
 	id uuid primary key default uuid_generate_v4(),
-	title varchar(255) not null
+	title varchar(255) not null,
+	created_at timestamp with time zone default now(),
 );
 
-create table messages (
+create table voice_channels (
 	id uuid primary key default uuid_generate_v4(),
-	text text not null,
-	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-
-	room_id uuid not null,
-	user_id uuid not null,
-	
-	foreign key (room_id) references rooms (id),
-	foreign key (user_id) references users (id)
+	title varchar(255) not null,
+	created_at timestamp with time zone default now(),
 );
 
-create table room_members (
+create table voice_channels_users(
 	user_id uuid not null,
-	room_id uuid not null,
+	voice_room_id uuid not null,
+	created_at timestamp with time zone default now(),
 	
-	foreign key (user_id) references users (id),
-	foreign key (room_id) references rooms (id),
-
-	primary key(user_id, room_id)
+	foreign key(user_id) references users(id),
+	foreign key(voice_room_id) references voice_rooms(id),
+	
+	primary key(user_id, voice_room_id)
 );
--- server/db/migrations/000003_create_voice_rooms.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE voice_rooms CASCADE;
-DROP TABLE voice_rooms_members CASCADE;
