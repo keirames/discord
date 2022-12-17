8afa269a55ab3c494b039041af17c06b8a8b4e42
khang nguyen <keirames.l3@gmail.com>
2022-12-17 17:37:07 +0700

    last

---
+++ app/src/pages/api/auth/sign-in.ts
@@ -0,0 +1,8 @@
+const signIn = () => {};
+
+const examples = async (req: NextApiRequest, res: NextApiResponse) => {
+  const examples = await prisma.example.findMany();
+  res.status(200).json(examples);
+};
+
+export default SignIn;
