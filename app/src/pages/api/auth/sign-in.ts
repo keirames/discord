dfc7cf78a8b726235d83c4d9932b08f0e9351acd
khang nguyen <keirames.l3@gmail.com>
2022-12-17 17:37:47 +0700

    remove trpc (spike in cpu & laggy vscode scare me)

--- app/src/pages/api/auth/sign-in.ts
+++
@@ -1,8 +0,0 @@
-const signIn = () => {};
-
-const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
-};
-
-export default SignIn;
